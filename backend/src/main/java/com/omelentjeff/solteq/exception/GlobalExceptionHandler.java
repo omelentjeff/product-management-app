1232e46d95160c787452b3d3918607b0819ebd73
Otto Melentjeff <otto.melentjeff@tuni.fi>
2024-10-16 15:45:44 +0300

    create a handler for ProductNotFoundException

--- backend/src/main/java/com/omelentjeff/solteq/exception/GlobalExceptionHandler.java
+++ backend/src/main/java/com/omelentjeff/solteq/exception/GlobalExceptionHandler.java
@@ -1,7 +1,19 @@
 package com.omelentjeff.solteq.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(ProductNotFoundException.class)
+    public ResponseEntity<ErrorResponse> handleProductNotFound(ProductNotFoundException ex) {
+        ErrorResponse error = new ErrorResponse();
+        error.setStatus(HttpStatus.NOT_FOUND.value());
+        error.setMessage(ex.getMessage());
+        error.setTimeStamp(System.currentTimeMillis());
+        return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
+    }
 }
