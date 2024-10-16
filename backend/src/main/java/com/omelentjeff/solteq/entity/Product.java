ee8c1289a8df2ab19398b4f65ef0d26113f90552
Otto Melentjeff <otto.melentjeff@tuni.fi>
2024-10-16 13:58:58 +0300

    backend FEAT: implement Product entity properties and annotations

--- backend/src/main/java/com/omelentjeff/solteq/entity/Product.java
+++ backend/src/main/java/com/omelentjeff/solteq/entity/Product.java
@@ -1,4 +1,38 @@
 package com.omelentjeff.solteq.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
 public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String manufacturer;
+
+    private String name;
+
+    private BigDecimal weight;
+
+    private int caloriesPer100g;
+
+    private int kilojoulesPer100g;
+
+    private String photoUrl;
+
+
 }
