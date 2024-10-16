1b841a8901b45256d790cb575aeb56b77f843c7d
Otto Melentjeff <otto.melentjeff@tuni.fi>
2024-10-16 17:57:12 +0300

    backend REMOVE: singleton instance of the mapper

--- backend/src/main/java/com/omelentjeff/solteq/mapper/ProductMapper.java
+++ backend/src/main/java/com/omelentjeff/solteq/mapper/ProductMapper.java
@@ -11,8 +11,6 @@ import org.mapstruct.factory.Mappers;
 @Mapper(componentModel = "spring")
 public interface ProductMapper {
 
-    ProductMapper INSTANCE = Mappers.getMapper(ProductMapper.class);
-
     ProductDTO toDTO(Product product);
 
     Product toEntity(ProductDTO productDTO);
