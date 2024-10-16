a68061c67e110aa12c8bd750c5e7956be479c219
Otto Melentjeff <otto.melentjeff@tuni.fi>
2024-10-16 14:13:48 +0300

    backend FEAT: annotate the ProductService class and inject the ProductRepository

--- backend/src/main/java/com/omelentjeff/solteq/service/ProductService.java
+++ backend/src/main/java/com/omelentjeff/solteq/service/ProductService.java
@@ -1,7 +1,11 @@
 package com.omelentjeff.solteq.service;
 
+import com.omelentjeff.solteq.repository.ProductRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class ProductService {
+    private final ProductRepository productRepository;
 }
