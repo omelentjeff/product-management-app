40427f4d466336fa05ff8557a052978bce331d64
Otto Melentjeff <otto.melentjeff@tuni.fi>
2024-10-16 14:09:36 +0300

    backend FEAT: create a custom search method in the ProductRepository that will search based on name, manufacturer and gtin

--- backend/src/main/java/com/omelentjeff/solteq/repository/ProductRepository.java
+++ backend/src/main/java/com/omelentjeff/solteq/repository/ProductRepository.java
@@ -1,10 +1,13 @@
 package com.omelentjeff.solteq.repository;
 
 import com.omelentjeff.solteq.entity.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Integer> {
+    Page<Product> findByNameContainingIgnoreCaseOrManufacturerContainingIgnoreCaseOrGtinIgnoreCase(String name, String manufacturer, String gtin, Pageable pageable);
 
 }
