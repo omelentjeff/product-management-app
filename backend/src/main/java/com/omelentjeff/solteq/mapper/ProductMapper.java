8cdbb6ae411e36459ac4931b6a909ad844aebdcc
Otto Melentjeff <otto.melentjeff@tuni.fi>
2024-10-16 17:54:30 +0300

    backend FEAT: create a mapper method to update (PATCH) a product using MapStruct

--- backend/src/main/java/com/omelentjeff/solteq/mapper/ProductMapper.java
+++ backend/src/main/java/com/omelentjeff/solteq/mapper/ProductMapper.java
@@ -2,7 +2,10 @@ package com.omelentjeff.solteq.mapper;
 
 import com.omelentjeff.solteq.dto.ProductDTO;
 import com.omelentjeff.solteq.entity.Product;
+import org.mapstruct.BeanMapping;
 import org.mapstruct.Mapper;
+import org.mapstruct.MappingTarget;
+import org.mapstruct.NullValuePropertyMappingStrategy;
 import org.mapstruct.factory.Mappers;
 
 @Mapper(componentModel = "spring")
@@ -13,4 +16,7 @@ public interface ProductMapper {
     ProductDTO toDTO(Product product);
 
     Product toEntity(ProductDTO productDTO);
+
+    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    void updateProductFromDto(ProductDTO dto, @MappingTarget Product entity);
 }
