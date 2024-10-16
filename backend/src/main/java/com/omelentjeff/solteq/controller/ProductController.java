c81dcca7fa26d3cfa108ccd680636857325debaf
Otto Melentjeff <otto.melentjeff@tuni.fi>
2024-10-16 14:58:39 +0300

    backend FEAT: create a controller method for fetching all products

--- backend/src/main/java/com/omelentjeff/solteq/controller/ProductController.java
+++ backend/src/main/java/com/omelentjeff/solteq/controller/ProductController.java
@@ -1,9 +1,28 @@
 package com.omelentjeff.solteq.controller;
 
+import com.omelentjeff.solteq.dto.ProductDTO;
+import com.omelentjeff.solteq.service.ProductService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("api/v1/products")
+@RequiredArgsConstructor
 public class ProductController {
+
+    private final ProductService productService;
+
+    @GetMapping({"/", ""})
+    public ResponseEntity<Page<ProductDTO>> getAllProducts(
+            @PageableDefault(size = 10, sort = "id", direction = Sort.Direction.ASC) Pageable pageable) {
+        Page<ProductDTO> products = productService.getAllProducts(pageable);
+        return ResponseEntity.ok(products);
+    }
 }
