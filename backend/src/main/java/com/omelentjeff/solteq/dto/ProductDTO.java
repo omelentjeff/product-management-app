934e116438e37a5d9eff7363d3f7140bc0b071b1
Otto Melentjeff <otto.melentjeff@tuni.fi>
2024-10-16 14:15:44 +0300

    backend FEAT: create a dto class for the product

---
+++ backend/src/main/java/com/omelentjeff/solteq/dto/ProductDTO.java
@@ -0,0 +1,11 @@
+package com.omelentjeff.solteq.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ProductDTO {
+}
