ff9039a45b68ebe3ccd230ba9751115fbbb4ea6a
Otto Melentjeff <otto.melentjeff@tuni.fi>
2024-10-16 14:18:36 +0300

    backend FEAT: implement the DTO properties

--- backend/src/main/java/com/omelentjeff/solteq/dto/ProductDTO.java
+++ backend/src/main/java/com/omelentjeff/solteq/dto/ProductDTO.java
@@ -4,8 +4,19 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.math.BigDecimal;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class ProductDTO {
+
+    private Integer id;
+    private String manufacturer;
+    private String name;
+    private BigDecimal weight;
+    private int caloriesPer100g;
+    private int kilojoulesPer100g;
+    private String photoUrl;
+    private String gtin;
 }
