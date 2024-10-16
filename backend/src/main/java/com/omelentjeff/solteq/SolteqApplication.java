711e456a19c5f9aae8f70ffbae9ad3381e8867f4
Otto Melentjeff <otto.melentjeff@tuni.fi>
2024-10-16 13:40:12 +0300

    create initial project structure for backend folder

---
+++ backend/src/main/java/com/omelentjeff/solteq/SolteqApplication.java
@@ -0,0 +1,13 @@
+package com.omelentjeff.solteq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SolteqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SolteqApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/omelentjeff/solteq/SolteqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.omelentjeff.solteq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SolteqApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
