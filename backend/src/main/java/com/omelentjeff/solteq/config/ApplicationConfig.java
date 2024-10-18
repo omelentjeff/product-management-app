0e7c28375bd3d159a2937eda3b9d8334a493f323
Otto Melentjeff <otto.melentjeff@tuni.fi>
2024-10-18 13:03:14 +0300

    backend FEAT: create ApplicationConfig class to hold some beans

---
+++ backend/src/main/java/com/omelentjeff/solteq/config/ApplicationConfig.java
@@ -0,0 +1,9 @@
+package com.omelentjeff.solteq.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@RequiredArgsConstructor
+public class ApplicationConfig {
+}
