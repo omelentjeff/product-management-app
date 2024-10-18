4fd3f00734f36707a963627c89788d6c2618ac3e
Otto Melentjeff <otto.melentjeff@tuni.fi>
2024-10-18 12:36:44 +0300

    backend FEAT: add role property to UserEntity

--- backend/src/main/java/com/omelentjeff/solteq/entity/UserEntity.java
+++ backend/src/main/java/com/omelentjeff/solteq/entity/UserEntity.java
@@ -26,6 +26,7 @@ public class UserEntity implements UserDetails {
     private Long id;
     private String username;
     private String password;
+    private Role role;
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
