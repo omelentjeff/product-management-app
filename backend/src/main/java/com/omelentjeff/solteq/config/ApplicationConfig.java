723c5706cefc9b07b86b87e3f9cbda7713c6e6e8
Otto Melentjeff <otto.melentjeff@tuni.fi>
2024-10-18 13:04:35 +0300

    backend FEAT: create a bean that provides an implementation for UserDetailsService

--- backend/src/main/java/com/omelentjeff/solteq/config/ApplicationConfig.java
+++ backend/src/main/java/com/omelentjeff/solteq/config/ApplicationConfig.java
@@ -2,11 +2,20 @@ package com.omelentjeff.solteq.config;
 
 import com.omelentjeff.solteq.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
 @Configuration
 @RequiredArgsConstructor
 public class ApplicationConfig {
 
     private final UserRepository repository;
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return username -> repository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+    }
 }
