4140beaf0dc75016344224bea713f9b78bbda108
Otto Melentjeff <otto.melentjeff@tuni.fi>
2024-10-18 16:35:13 +0300

    backend FEAT: create AuthController for authentication related endpoints

---
+++ backend/src/main/java/com/omelentjeff/solteq/auth/AuthController.java
@@ -0,0 +1,11 @@
+package com.omelentjeff.solteq.auth;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/auth")
+@RequiredArgsConstructor
+public class AuthController {
+}
