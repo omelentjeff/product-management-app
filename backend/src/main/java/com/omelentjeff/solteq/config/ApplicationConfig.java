aefd05c9770fec15f979bbb6279479f43df21079
Otto Melentjeff <otto.melentjeff@tuni.fi>
2024-10-18 13:03:49 +0300

    backend FEAT: inject userRepository into ApplicationConfig

--- backend/src/main/java/com/omelentjeff/solteq/config/ApplicationConfig.java
+++ backend/src/main/java/com/omelentjeff/solteq/config/ApplicationConfig.java
@@ -1,9 +1,12 @@
 package com.omelentjeff.solteq.config;
 
+import com.omelentjeff.solteq.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @RequiredArgsConstructor
 public class ApplicationConfig {
+
+    private final UserRepository repository;
 }
