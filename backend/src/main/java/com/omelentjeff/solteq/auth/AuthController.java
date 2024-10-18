0d956fa7d07f7a2b7d64f6f77566b18bb32c572b
Otto Melentjeff <otto.melentjeff@tuni.fi>
2024-10-18 16:42:52 +0300

    backend FEAT: inject AuthenticationService into AuthController

--- backend/src/main/java/com/omelentjeff/solteq/auth/AuthController.java
+++ backend/src/main/java/com/omelentjeff/solteq/auth/AuthController.java
@@ -11,6 +11,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class AuthController {
 
+    private final AuthenticationService service;
+
     @PostMapping("/register")
     public ResponseEntity<AuthenticationResponse> register(
             @RequestBody RegisterRequest request
