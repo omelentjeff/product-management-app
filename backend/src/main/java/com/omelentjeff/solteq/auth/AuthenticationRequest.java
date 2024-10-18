50f3a889d56e60bcab49d3426d9985695a42776f
Otto Melentjeff <otto.melentjeff@tuni.fi>
2024-10-18 16:42:07 +0300

    backend FEAT: create AuthenticationService class

--- backend/src/main/java/com/omelentjeff/solteq/auth/AuthenticationRequest.java
+++ backend/src/main/java/com/omelentjeff/solteq/auth/AuthenticationRequest.java
@@ -10,7 +10,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class AuthenticationRequest {
-    
+
     private String username;
     private String password;
 }
