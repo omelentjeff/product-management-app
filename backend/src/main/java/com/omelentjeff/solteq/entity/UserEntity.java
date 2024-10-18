b2ff4eba22078476b2eafed6a5a6003a5b96c2c9
Otto Melentjeff <otto.melentjeff@tuni.fi>
2024-10-18 12:38:43 +0300

    backend FEAT:  implement getAuthorities -method in UserEntity

--- backend/src/main/java/com/omelentjeff/solteq/entity/UserEntity.java
+++ backend/src/main/java/com/omelentjeff/solteq/entity/UserEntity.java
@@ -7,9 +7,11 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
+import java.util.List;
 
 @Data
 @Builder
@@ -23,13 +25,13 @@ public class UserEntity implements UserDetails {
     private Long id;
     private String username;
     private String password;
-    
+
     @Enumerated(EnumType.STRING)
     private Role role;
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return null;
+        return List.of(new SimpleGrantedAuthority(role.name()));
     }
 
     @Override
