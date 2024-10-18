90aea55e3cc9006c19dcf755ecdc9ccc4263957a
Otto Melentjeff <otto.melentjeff@tuni.fi>
2024-10-18 16:46:17 +0300

    backend FEAT: inject UserRepository into AuthenticationService

--- backend/src/main/java/com/omelentjeff/solteq/auth/AuthenticationService.java
+++ backend/src/main/java/com/omelentjeff/solteq/auth/AuthenticationService.java
@@ -1,11 +1,14 @@
 package com.omelentjeff.solteq.auth;
 
+import com.omelentjeff.solteq.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class AuthenticationService {
+
+    private final UserRepository userRepository;
 
     public AuthenticationResponse register(RegisterRequest request) {
         return null;
