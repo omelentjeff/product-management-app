0758c12aaad0a091d49f8e669c2b18a86b0053a4
Otto Melentjeff <otto.melentjeff@tuni.fi>
2024-10-18 16:43:47 +0300

    backend FEAT: implement AuthController register method

--- backend/src/main/java/com/omelentjeff/solteq/auth/AuthController.java
+++ backend/src/main/java/com/omelentjeff/solteq/auth/AuthController.java
@@ -1,6 +1,7 @@
 package com.omelentjeff.solteq.auth;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,7 +18,7 @@ public class AuthController {
     public ResponseEntity<AuthenticationResponse> register(
             @RequestBody RegisterRequest request
     ) {
-        //
+        return ResponseEntity.ok(service.register(request));
     }
 
     @PostMapping("/authenticate")
