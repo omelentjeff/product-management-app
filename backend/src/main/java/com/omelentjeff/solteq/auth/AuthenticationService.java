17fb1e8265442091e8cb1a0727b99bd2700c2fba
Otto Melentjeff <otto.melentjeff@tuni.fi>
2024-10-18 16:48:08 +0300

    backend FEAT: inject JwtService into AuthenticationService

--- backend/src/main/java/com/omelentjeff/solteq/auth/AuthenticationService.java
+++ backend/src/main/java/com/omelentjeff/solteq/auth/AuthenticationService.java
@@ -1,6 +1,7 @@
 package com.omelentjeff.solteq.auth;
 
 import com.omelentjeff.solteq.repository.UserRepository;
+import com.omelentjeff.solteq.service.JwtService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -11,6 +12,7 @@ public class AuthenticationService {
 
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
+    private final JwtService jwtService;
 
     public AuthenticationResponse register(RegisterRequest request) {
         return null;
