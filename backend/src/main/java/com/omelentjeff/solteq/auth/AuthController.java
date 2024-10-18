06bcc8b9d63ac74ca14767dbd76826f83b84f646
Otto Melentjeff <otto.melentjeff@tuni.fi>
2024-10-18 16:36:18 +0300

    backend FEAT: create a register POST endpoint

--- backend/src/main/java/com/omelentjeff/solteq/auth/AuthController.java
+++ backend/src/main/java/com/omelentjeff/solteq/auth/AuthController.java
@@ -1,6 +1,8 @@
 package com.omelentjeff.solteq.auth;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,4 +10,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1/auth")
 @RequiredArgsConstructor
 public class AuthController {
+
+    @PostMapping("/register")
+    public ResponseEntity<AuthenticationResponse> register(
+            @RequestBody RegisterRequest request
+    ) {
+        //
+    }
 }
