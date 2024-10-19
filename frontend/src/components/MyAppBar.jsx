ac3b1639ec844860c4525a8131808a0ec5151654
Otto Melentjeff <otto.melentjeff@tuni.fi>
2024-10-19 14:20:27 +0300

    frontend FEAT: implement a logout feature to the app bar

--- frontend/src/components/MyAppBar.jsx
+++ frontend/src/components/MyAppBar.jsx
@@ -1,10 +1,19 @@
 import * as React from "react";
+import { Link as RouterLink, useNavigate } from "react-router-dom";
 import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
-import Typography from "@mui/material/Typography";
+import { Typography, Button } from "@mui/material";
+import authService from "../authService";
 
 export default function MyAppBar() {
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    authService.logout();
+    navigate("/login");
+  };
+
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static">
@@ -16,6 +25,9 @@ export default function MyAppBar() {
           >
             Product API
           </Typography>
+          <Button color="inherit" onClick={handleLogout}>
+            Logout
+          </Button>
         </Toolbar>
       </AppBar>
     </Box>
