c919a88cc3f797241b57ed30d1900f14a85ccf3e
Otto Melentjeff <otto.melentjeff@tuni.fi>
2024-10-19 09:02:24 +0300

    frontend FEAT: add routing to App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,15 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./components/Login";
+
 function App() {
-  return <SignIn />;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
