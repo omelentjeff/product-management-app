a8418c793f82bf8a42ed82a649873607b4afb2bb
Otto Melentjeff <otto.melentjeff@tuni.fi>
2024-10-19 09:07:48 +0300

    frontend FEAT: add route for Signup component in App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./components/Login";
+import Signup from "./components/Signup";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
       </Routes>
     </Router>
   );
