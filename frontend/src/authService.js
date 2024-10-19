5a84bb39c85e897f470ede0bb22f16a818e6dad2
Otto Melentjeff <otto.melentjeff@tuni.fi>
2024-10-19 08:53:33 +0300

    frontend FEAT: implement register function and store jwt in local storage

--- frontend/src/authService.js
+++ frontend/src/authService.js
@@ -1,3 +1,23 @@
 import axios from "axios";
 
 const API_URL = "http://localhost:8080/api/v1/auth";
+
+const register = async (username, password) => {
+  try {
+    const response = await axios.post(`${API_URL}/register`, {
+      username,
+      password,
+    });
+
+    if (response.data.token) {
+      localStorage.setItem("token", JSON.stringify(response.data.token)); // Store JWT token
+      return response.data;
+    }
+
+    throw new Error("Registration failed");
+  } catch (error) {
+    throw (
+      error.response?.data || new Error("An error occurred during registration")
+    );
+  }
+};
