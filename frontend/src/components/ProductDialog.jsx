80dcadac0e3ce700fb554fa9c08bf1996c45963c
Otto Melentjeff <otto.melentjeff@tuni.fi>
2024-10-19 15:02:17 +0300

    frontend FEAT: dialog displays product name as title on opening

--- frontend/src/components/ProductDialog.jsx
+++ frontend/src/components/ProductDialog.jsx
@@ -1,13 +1,15 @@
-import * as React from "react";
+import React, { useState, useEffect } from "react";
 import Button from "@mui/material/Button";
 import { styled } from "@mui/material/styles";
 import Dialog from "@mui/material/Dialog";
 import DialogTitle from "@mui/material/DialogTitle";
 import DialogContent from "@mui/material/DialogContent";
-import DialogActions from "@mui/material/DialogActions";
 import IconButton from "@mui/material/IconButton";
 import CloseIcon from "@mui/icons-material/Close";
 import Typography from "@mui/material/Typography";
+import LocationOnIcon from "@mui/icons-material/LocationOn";
+import { Box, Grid, CircularProgress } from "@mui/material";
+import { fetchSingleData } from "../apiService";
 
 const BootstrapDialog = styled(Dialog)(({ theme }) => ({
   "& .MuiDialogContent-root": {
@@ -18,63 +20,82 @@ const BootstrapDialog = styled(Dialog)(({ theme }) => ({
   },
 }));
 
-export default function CustomizedDialogs() {
-  const [open, setOpen] = React.useState(false);
+export default function StationDialog({ product, text }) {
+  const [open, setOpen] = useState(false);
+  const [productDetails, setProductDetails] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
 
-  const handleClickOpen = () => {
+  const handleClickOpen = async () => {
     setOpen(true);
+    setIsLoading(true);
+
+    try {
+      const details = await fetchSingleData(product.id);
+      setProductDetails(details);
+    } catch (error) {
+      console.error("Failed to fetch product details:", error);
+    } finally {
+      setIsLoading(false);
+    }
   };
+
   const handleClose = () => {
     setOpen(false);
+    setProductDetails(null);
   };
 
   return (
     <React.Fragment>
       <Button variant="outlined" onClick={handleClickOpen}>
-        Open dialog
+        {text}
       </Button>
       <BootstrapDialog
         onClose={handleClose}
         aria-labelledby="customized-dialog-title"
         open={open}
+        fullWidth={true}
+        maxWidth="md"
       >
-        <DialogTitle sx={{ m: 0, p: 2 }} id="customized-dialog-title">
-          Modal title
-        </DialogTitle>
-        <IconButton
-          aria-label="close"
-          onClick={handleClose}
-          sx={(theme) => ({
-            position: "absolute",
-            right: 8,
-            top: 8,
-            color: theme.palette.grey[500],
-          })}
-        >
-          <CloseIcon />
-        </IconButton>
-        <DialogContent dividers>
-          <Typography gutterBottom>
-            Cras mattis consectetur purus sit amet fermentum. Cras justo odio,
-            dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta
-            ac consectetur ac, vestibulum at eros.
-          </Typography>
-          <Typography gutterBottom>
-            Praesent commodo cursus magna, vel scelerisque nisl consectetur et.
-            Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor
-            auctor.
-          </Typography>
-          <Typography gutterBottom>
-            Aenean lacinia bibendum nulla sed consectetur. Praesent commodo
-            cursus magna, vel scelerisque nisl consectetur et. Donec sed odio
-            dui. Donec ullamcorper nulla non metus auctor fringilla.
-          </Typography>
-        </DialogContent>
-        <DialogActions>
-          <Button autoFocus onClick={handleClose}>
-            Save changes
-          </Button>
-        </DialogActions>
+        {isLoading ? (
+          <Box
+            display="flex"
+            justifyContent="center"
+            alignItems="center"
+            sx={{ height: "300px" }}
+          >
+            <CircularProgress />
+          </Box>
+        ) : (
+          productDetails && (
+            <>
+              <DialogTitle
+                sx={{
+                  m: 0,
+                  p: 2,
+                  textAlign: "center",
+                  fontWeight: "bold",
+                  fontSize: 24,
+                  color: "#333",
+                }}
+                id="customized-dialog-title"
+              >
+                {productDetails.name}
+              </DialogTitle>
+              <IconButton
+                aria-label="close"
+                onClick={handleClose}
+                sx={{
+                  position: "absolute",
+                  right: 8,
+                  top: 8,
+                  color: (theme) => theme.palette.grey[500],
+                }}
+              >
+                <CloseIcon />
+              </IconButton>
+            </>
+          )
+        )}
       </BootstrapDialog>
     </React.Fragment>
   );
