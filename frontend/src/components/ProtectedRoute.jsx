549983fcfa6cb6209960d21c0a5458109639e013
Otto Melentjeff <otto.melentjeff@tuni.fi>
2024-10-19 09:16:46 +0300

    frontend FEAT: implement ProtectedRoute component

--- frontend/src/components/ProtectedRoute.jsx
+++ frontend/src/components/ProtectedRoute.jsx
@@ -1,5 +1,17 @@
-export default function ProtectedRoute({ element }) {
-  const isAuthenticated = false; // todo: check if user is authenticated
+import React from "react";
+import { Navigate } from "react-router-dom";
+import authService from "../authService";
 
-  return isAuthenticated ? element : <Navigate to="/login" />;
+export default function ProtectedRoute({ children }) {
+  const isAuthenticated = authService.getCurrentUser();
+
+  console.log("isAuthenticated", isAuthenticated);
+
+  if (!isAuthenticated) {
+    // if user isn't authenticated, redirect the user to login page
+    return <Navigate to="/login" />;
+  }
+
+  // if user is authenticated, redirect the user to the protected page
+  return children;
 }
