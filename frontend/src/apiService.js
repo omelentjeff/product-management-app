97ed45d7290f578438ae829a3eeb99326463a430
Otto Melentjeff <otto.melentjeff@tuni.fi>
2024-10-19 10:09:00 +0300

    frontend FEAT: create api function to fetch single page of data

--- frontend/src/apiService.js
+++ frontend/src/apiService.js
@@ -1,1 +1,16 @@
 const baseUrl = import.meta.env.VITE_API_URL;
+
+export const fetchData = async (page = 0, pageSize = 10, sort = "") => {
+  try {
+    console.log("API Base URL:", baseUrl);
+    const response = await fetch(
+      `${baseUrl}?page=${page}&size=${pageSize}&sort=${sort}`
+    );
+
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error(`Error fetching data:`, error);
+    throw error;
+  }
+};
