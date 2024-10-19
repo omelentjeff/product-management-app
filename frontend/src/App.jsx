d84e3ea121dc446de817915a970482300c8e9493
Otto Melentjeff <otto.melentjeff@tuni.fi>
2024-10-19 13:26:55 +0300

    frontend REFACTOR: render ProductTable directly instead of Dashboard

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,10 +4,13 @@ import Login from "./components/Login";
 import Signup from "./components/Signup";
 import ProtectedRoute from "./components/ProtectedRoute";
 import Dashboard from "./components/Dashboard";
+import MyAppBar from "./components/MyAppBar";
+import ProductTable from "./components/ProductTable";
 
 function App() {
   return (
     <Router>
+      <MyAppBar />
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
@@ -15,7 +18,7 @@ function App() {
           path="/dashboard"
           element={
             <ProtectedRoute>
-              <Dashboard />
+              <ProductTable />
             </ProtectedRoute>
           }
         />
