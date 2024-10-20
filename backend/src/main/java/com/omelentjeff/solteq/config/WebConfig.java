e83e666feaac09cc62e61891a8280b6f4f4ee619
Otto Melentjeff <otto.melentjeff@tuni.fi>
2024-10-20 09:50:15 +0300

    backend FEAT: update cors mapping by allowing PATCH methods

--- backend/src/main/java/com/omelentjeff/solteq/config/WebConfig.java
+++ backend/src/main/java/com/omelentjeff/solteq/config/WebConfig.java
@@ -14,7 +14,7 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("http://localhost:3000", "http://localhost:5173")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedMethods("GET", "POST", "PATCH", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
     }
