ac5f55aab90af04d1de2ab02e4d58a3c891ca5dd
Otto Melentjeff <otto.melentjeff@tuni.fi>
2024-10-21 06:41:13 +0300

    backend FEAT: create a handler for MethodArgumentNotValidException

--- backend/src/main/java/com/omelentjeff/solteq/exception/GlobalExceptionHandler.java
+++ backend/src/main/java/com/omelentjeff/solteq/exception/GlobalExceptionHandler.java
@@ -2,8 +2,14 @@ package com.omelentjeff.solteq.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.time.Instant;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -28,6 +34,28 @@ public class GlobalExceptionHandler {
                 .message(ex.getMessage())
                 .timeStamp(System.currentTimeMillis())
                 .build();
+        return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponse> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        List<String> details = ex.getBindingResult()
+                .getAllErrors()
+                .stream()
+                .map(error -> {
+                    String fieldName = ((FieldError) error).getField();
+                    String errorMessage = error.getDefaultMessage();
+                    return fieldName + ": " + errorMessage;
+                })
+                .collect(Collectors.toList());
+
+        ErrorResponse error = ErrorResponse.builder()
+                .status(HttpStatus.BAD_REQUEST.value())
+                .message("Validation failed!")
+                .timeStamp(System.currentTimeMillis())
+                .details(details)
+                .build();
+
         return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
     }
 }
