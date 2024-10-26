28424b5f334c223e1e5251bf9a896fbc5654cf11
Otto Melentjeff <otto.melentjeff@tuni.fi>
2024-10-26 21:18:25 +0300

    insert initial data to database

--- db-init-scripts/db-init.sql
+++ db-init-scripts/db-init.sql
@@ -59,3 +59,26 @@ DROP USER IF EXISTS 'product_admin'@'%';
 CREATE USER 'product_admin'@'%' IDENTIFIED BY 'productPW1!';
 GRANT ALL PRIVILEGES ON `products_db`.* TO 'product_admin'@'%';
 FLUSH PRIVILEGES;
+
+ALTER DATABASE products_db CHARACTER SET utf8 COLLATE utf8_general_ci;
+
+
+INSERT INTO `nutritional_fact` (`calories`, `kilojoules`, `fat`, `carbohydrates`, `sugars`, `polyols`, `fibers`, `protein`, `sodium`) VALUES
+(263, 1097, 2.9, 94, 0.24, 88, 0.93, 0.14, 0.03),
+(269, 1118, 3.8, 93, 0, 88, 0.99, 0, 0.03),
+(271, 1131, 3.6, 94, 0, 89, 0.94, 0, 0.03),
+(268, 1115, 3.9, 92, 0, 87, 0.95, 0, 0.19),
+(275, 1145, 3.9, 93, 0, 0, 0, 0, 0.9),
+(273, 1139, 4.3, 93, 0, 88, 0.93, 0, 0.03),
+(272, 1134, 4.2, 93, 0.11, 88, 0.93, 0.12, 0.03),
+(274, 1140, 4.4, 93, 0.11, 88, 0.94, 0, 0.03);
+
+INSERT INTO `product` (`manufacturer`, `name`, `weight`, `nutritional_fact_id`, `photo_url`, `gtin`) VALUES
+('Cloetta', 'Läkerol Dents Raspberry Salmiak', 36, 1, 'uploads/images/raspberry-salmiak.webp', '6420256016909'),
+('Cloetta', 'Läkerol Dents Menthol', 36, 2, 'uploads/images/menthol.webp', '6420256001523'),
+('Cloetta', 'Läkerol Dents Sweetmint', 36, 3, 'uploads/images/sweetmint.webp', '6420256002131'),
+('Cloetta', 'Läkerol Dents Apple Fresh White', 36, 4, 'uploads/images/apple-fresh-white.webp', '6420256012512'),
+('Cloetta', 'Läkerol Dents Menthol (85g)', 85, 5, 'uploads/images/menthol-large.webp', '6420256013519'),
+('Cloetta', 'Läkerol Dents Licorice Vanilla', 85, 6, 'uploads/images/licorice-vanilla.webp', '6420256012918'),
+('Cloetta', 'Läkerol Dents Strawberry Cream', 85, 7, 'uploads/images/strawberry-cream.webp', '6420256012901'),
+('Cloetta', 'Läkerol Dents Sweet Mango', 85, 8, 'uploads/images/sweet-mango.webp', '6420256016299');
