9843efb71af478450cedd39b883aae5dff25ed21
Otto Melentjeff <otto.melentjeff@tuni.fi>
2024-10-27 18:50:02 +0200

    frontend FIX: route issue redirected to non existent route

--- frontend/src/components/Signup.jsx
+++ frontend/src/components/Signup.jsx
@@ -236,7 +236,7 @@ export default function Signup() {
           </Button>
           <Grid container justifyContent="flex-end">
             <Grid item>
-              <Link component={RouterLink} to="/login" variant="body2">
+              <Link component={RouterLink} to="/" variant="body2">
                 Already have an account? Sign in
               </Link>
             </Grid>
